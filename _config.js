ed87ccc1fb8f8eb522e4d87f5439e4a582c0d4b2
Albert Byrone <albertbyrone1677@gmail.com>
2022-10-24 11:46:33 +0300

    update the databse configurations

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://albertbyrone:e6BH50fzOVlg5hRj@gallerycluster.rpn5xgz.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://albertbyrone:e6BH50fzOVlg5hRj@gallerycluster.rpn5xgz.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://albertbyrone:e6BH50fzOVlg5hRj@gallerycluster.rpn5xgz.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://albertbyrone:RjA29ZTyUvsrux5a@gallery.x5pk4v6.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://albertbyrone:RjA29ZTyUvsrux5a@gallery.x5pk4v6.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://albertbyrone:RjA29ZTyUvsrux5a@gallery.x5pk4v6.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
